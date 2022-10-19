28fdf7daf4897609096dcc292ae87123bf2b4386
abunaser1985 <112996763+abunaser1985@users.noreply.github.com>
2022-10-18 21:38:11 -0500

    new tc

---
+++ src/javaone/trailtwo.java
@@ -0,0 +1,26 @@
+package javaone;
+
+public class trailtwo {
+
+	public static void main(String[] args) {
+		
+		printme();
+		printme1();
+		printme2();
+	}
+    public static void printme() {
+	
+    	System.out.println("Test");
+}
+    
+    public static void printme1() {
+    	
+    	System.out.println("Test");
+}
+    
+    public static void printme2() {
+    	
+    	System.out.println("Test");
+}
+    
+}
